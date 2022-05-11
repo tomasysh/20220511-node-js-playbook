3350561ea7b9563f018e73884ab7dc483bc08b9c
tomasysh <tomasysh@gmail.com>
2022-05-11 11:33:57 +0800

    補充: coding style

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+// 第一個區塊 內建模組
+const path = require('path');
+const http = require('http');
+
+// 第二個區塊 第三方模組(套件)
+
+// 第三個區塊 自建模組
 const hello = require('./hello');
+
+////////////////////////////////////////////////////////////////
 
 hello.sayHello();
 
