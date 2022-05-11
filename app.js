deacbf1e984b370c80943b188111cb72f03c7e34
tomasysh <tomasysh@gmail.com>
2022-05-11 16:33:47 +0800

    3-1 使用 Express 創建 Web Server 並設定路由 && 3-2 使用 sendFile() response HTML 檔案（搭配內建模組 path）

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 // 第一個區塊 內建模組
-const http = require('http');
+const path = require('path');
 
 // 第二個區塊 第三方模組(套件)
+const express = require('express');
 
 // 第三個區塊 自建模組
 
 ////////////////////////////////////////////////////////////////
 
-const server = http.createServer((req, res) => {
-	// console.log('第一個參數是瀏覽器對 web server 的 request', req);
-	// console.log('第二個參數是 web 要response 給瀏覽器的內容', res);
-    if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' }); //MIME type
-        return res.end('<h1>This is Home page</h1>');
-    }
-    if (req.url === '/login') {
-        res.writeHead(200, { 'Content-Type': 'text/html' }); //MIME type
-        return res.end('<h1>This is Login page</h1>');
-    }
-	res.end();
+const app = express();
+
+// middleware
+app.use((req, res, next) => {
+	console.log('Hello!');
+    next();
 });
 
-server.listen(3000, () => {
-	console.log('running server on port 3000');
+app.use((req, res, next) => {
+	console.log('World!');
+    next();
+});
+
+app.get('/', (req, res) => {
+    // res.writeHead(200, { 'Content-Type': 'text/html' });
+    // res.write('<head><meta charset="utf-8" /></head>')
+    // res.write('<body>')
+    // res.write('<h1>這是首頁</h1>')
+    // res.write('</body>')
+    res.status(200)
+        .sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(3000, () => {
+	console.log('Web Server is running on port 3000');
 });
