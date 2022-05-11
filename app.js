e77a3b40a2231995f7e8124bc43e9ab8470a5d4f
tomasysh <tomasysh@gmail.com>
2022-05-11 14:45:24 +0800

    2-1 建立 Server 並監聽變化（ 內建模組 http）

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 // 第一個區塊 內建模組
-const path = require('path');
 const http = require('http');
 
 // 第二個區塊 第三方模組(套件)
-const cowsay = require('cowsay');
 
 // 第三個區塊 自建模組
-const hello = require('./hello');
 
 ////////////////////////////////////////////////////////////////
 
-let sentences = ['Hello', 'World', 'I\'m a cow.'];
+const server = http.createServer((req, res) => {
+	// console.log('第一個參數是瀏覽器對 web server 的 request', req);
+	// console.log('第二個參數是 web 要response 給瀏覽器的內容', res);
+    console.log('req url:', req.url);
+    if (req.url === '/login') {
+        return res.end('This is login page');
+    }
+	res.end();
+});
 
-sentences.forEach((sentence) => {
-    console.log(cowsay.say({
-        text : sentence,
-        e : "==",
-        T : "U "
-    }));
+server.listen(3000, () => {
+	console.log('running server on port 3000');
 });
