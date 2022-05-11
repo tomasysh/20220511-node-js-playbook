95f770eddf07149ac806cd3cfcf97f68a5783621
tomasysh <tomasysh@gmail.com>
2022-05-11 15:19:43 +0800

    2-2 回傳 HTTP 狀態碼與網頁內容

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const server = http.createServer((req, res) => {
 	// console.log('第二個參數是 web 要response 給瀏覽器的內容', res);
     console.log('req url:', req.url);
     if (req.url === '/login') {
-        return res.end('This is login page');
+        res.writeHead(200, { 'Content-Type': 'text/html' }); //MIME type
+        return res.end('<h1>This is Login page</h1>');
     }
 	res.end();
 });
