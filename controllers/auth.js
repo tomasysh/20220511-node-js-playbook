37aa728ba7d12f34d9272126303dd40cd25f2bd8
tomasysh <tomasysh@gmail.com>
2022-05-19 15:43:38 +0800

    6-2 登入功能(2) 實作 postLogin 控制

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,7 @@
+const User = require('../models/user');
+
+////////////////////////////////////////////////////////////////
+
 const getLogin = (req, res) => {
     res.status(200)
         .render('auth/login', {
@@ -8,11 +12,22 @@ const getLogin = (req, res) => {
 
 const postLogin = (req, res) => {
     const { email, password } = req.body;
-    if (email && password) {
-        res.redirect('/');
-    } else {
-        console.log('欄位尚未填寫完成！')
-    }
+    User.findOne({ where: { email }})
+        .then((user) => {
+            if (!user) {
+                console.log('login: 找不到此 user 或密碼錯誤');
+                return res.redirect('/login');
+            }
+            if (user.password === password) {
+                console.log('login: 成功');
+                return res.redirect('/')
+            } 
+            console.log('login: 找不到此 user 或密碼錯誤');
+            res.redirect('/login');
+        })
+        .catch((err) => {
+            console.log('login error:', err);
+        });
 };
 
 const postLogout = (req, res) => {
