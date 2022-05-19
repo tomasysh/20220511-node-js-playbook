1ae52d9a94178961a1a61fe1911aa6d842263532
tomasysh <tomasysh@gmail.com>
2022-05-19 09:56:58 +0800

    5-1.plus 重構專案路由

--- routes/shop.js
+++ routes/shop.js
@@ -1,45 +1,19 @@
-// 第一個區塊 內建模組
-const path = require('path');
-
-// 第二個區塊 第三方模組(套件)
 const express = require('express');
-const bodyParser = require('body-parser');
 
-// 第三個區塊 自建模組
-const authRoutes = require('./routes/auth'); 
-const shopRoutes = require('./routes/shop'); 
-const errorRoutes = require('./routes/404');
+////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////////
+const router = express.Router();
 
-const app = express();
-
-// middleware
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use((req, res, next) => {
-	console.log('Hello!');
-    next();
+router.get('/', (req, res) => {
+    res.status(200)
+        .render('index', {
+            path: '/',
+            pageTitle: 'Book Your Books online',
+            products
+        });
 });
 
-app.use((req, res, next) => {
-	console.log('World!');
-    next();
-});
-
-
-app.use(authRoutes);
-app.use(shopRoutes);
-app.use(errorRoutes);
-
-
-app.listen(3000, () => {
-	console.log('Web Server is running on port 3000');
-});
+module.exports = router;
 
 const products = [
     {
