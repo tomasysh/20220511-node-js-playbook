ddd48b769128a6f1b47e8a2e8513fc4a46c77487
tomasysh <tomasysh@gmail.com>
2022-05-20 15:56:53 +0800

    7-3 plus 快速註冊帳號

--- app.js
+++ app.js
@@ -50,9 +50,8 @@ app.use(errorRoutes);
 
 database
     // .sync()
-	.sync()
+	.sync({ force: true }) // 和 db 連線職，強制重設 db
 	.then((result) => {
-        // User.create({ displayName: 'Admin', email: 'admin@skoob.com', password: '11111111'});
         // Product.bulkCreate(products);
 		app.listen(port, () => {
 			console.log(`Web Server is running on port ${port}`);
