c81ce1e367212a228206d37af56555fa74825e9e
tomasysh <tomasysh@gmail.com>
2022-05-23 16:00:59 +0800

    8-5 路由守衛：阻止無權限使用者訪問路由

--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,15 @@
 const express = require('express');
 
 const shopController = require('../controllers/shop');
+const isLogin = require('../authGuard/isLogin');
 
 ////////////////////////////////////////////////////////////
 
 const router = express.Router();
 
 router.get('/', shopController.getIndex);
-router.get('/cart', shopController.getCart);
-router.post('/cart-add-item', shopController.postCartAddItem);
-router.post('/cart-delete-item', shopController.postCartDeleteItem);
+router.get('/cart', isLogin, shopController.getCart);
+router.post('/cart-add-item', isLogin, shopController.postCartAddItem);
+router.post('/cart-delete-item', isLogin, shopController.postCartDeleteItem);
 
 module.exports = router;
